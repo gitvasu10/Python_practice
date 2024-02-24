6bf67624ed07a2dd999361defdc0bb8b35d3e5ca
gitvasu10 <developervasu10@gmail.com>
2024-02-25 03:06:33 +0530

    Count of substring in a given string

--- test2.py
+++ test2.py
@@ -15,6 +15,16 @@ print("My name is " +name +" and age is = "+age)'''
 # print(testList)
 
 #Using enumerate() with lists
-testList = [1,2,3,4,5,6,7,8,9,0,9,9,7,10]
-for index, value in enumerate(testList):
-    print(f"Index = {index}, Value = {value}")
+# testList = [1,2,3,4,5,6,7,8,9,0,9,9,7,10]
+# for index, value in enumerate(testList):
+#     print(f"Index = {index}, Value = {value}")
+
+#--------------25/02/2024----------------------------------------
+
+name = input("Enter the name: ")
+substring = input("Which substring you want to search for: ")
+count = 0
+for i in range(len(name)-len(substring)+1):
+    if(name[i:].startswith(substring)):
+        count += 1
+print(f"The count of {substring} in {name} is {count}")
