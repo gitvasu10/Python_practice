cebc2b345984400a60283800174890f0f09dc935
gitvasu10 <developervasu10@gmail.com>
2024-02-25 01:51:37 +0530

    List manipulation

--- test2.py
+++ test2.py
@@ -1,6 +1,20 @@
 #print("Kumud Malasi")
-
-age = 24
+#-----------23/02/24----------------------
+'''age = 24
 name = "Shashwat"
 
-print("My name is " +name +" and age is = "+age)
+print("My name is " +name +" and age is = "+age)'''
+
+#--------------------------24/02/2024-----------------------------------
+
+# behaviour of python lists while deleting the values from them
+
+# testList = [1,2,3,4,5,6]
+# for i in testList:
+#     testList.remove(i)
+# print(testList)
+
+#Using enumerate() with lists
+testList = [1,2,3,4,5,6,7,8,9,0,9,9,7,10]
+for index, value in enumerate(testList):
+    print(f"Index = {index}, Value = {value}")
